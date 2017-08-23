b569c151d6156a3990886f5d3dfccbff0763a0fd
JJZi <huijieyep@shaw.ca>
2017-08-23 08:50:20 -0600

    fix manifest

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,8 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const config = functions.config();
-const nodemailer = require('nodemailer');
 
-admin.initializeApp(config.firebase);
-
-const gmailEmail = encodeURIComponent(functions.config().gmail.email);
-const gmailPassword = encodeURIComponent(functions.config().gmail.password);
-const mailTransport = nodemailer.createTransport(
-    `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
-
-
-exports.sendEmailConfirmation = functions.database.ref('/messages/{uId}').onWrite(event => {
-  const message = event.data.val();  
-  
-  const mailOptions = {
-    from: '<noreply@firebase.com>',
-    to: 'annsxie.web@gmail.com',
-    subject: 'new message received',
-    text: JSON.stringify(message)
-  };   
-   
-    return mailTransport.sendMail(mailOptions).then(() => {
-      console.log('Message confirmation email sent');
-   
-});
-});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
